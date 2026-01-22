495df57d93b070f68604aa4336d2a3d4de5d0b7e
Saul-Ortega <saulalbertoortegarodriguez@gmail.com>
2026-01-22 11:03:55 +0000

    refactor: Modificación de archivo para que abra la ventana principal

--- application-desktop/main.cpp
+++ application-desktop/main.cpp
@@ -1,20 +1,12 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include "Applicationdao.h"
-#include <QString>
-#include <QList>
-#include "Application.h"
 
 int main(int argc, char *argv[])
 {
-    QList <Application> qq;
-    ApplicationDao t =ApplicationDao();
-
-    t.saveAll(qq);
     QApplication a(argc, argv);
-    //MainWindow w;
-    //w.show();
+    MainWindow w;
+    w.show();
     return a.exec();
 
 }
