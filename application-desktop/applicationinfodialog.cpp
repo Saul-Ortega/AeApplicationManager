25a961cc4047d64df54a85c2bf53415cb8c099c7
Saul-Ortega <saulalbertoortegarodriguez@gmail.com>
2026-02-06 16:01:52 +0000

    doc: Adición de comentario en ApplicationInfoDialog

--- application-desktop/applicationinfodialog.cpp
+++ application-desktop/applicationinfodialog.cpp
@@ -49,6 +49,11 @@ ApplicationInfoDialog::ApplicationInfoDialog(QWidget *parent)
         connect(thread, &QThread::finished, worker, &installerWorker::deleteLater);
         connect(thread, &QThread::finished, thread, &QThread::deleteLater);
 
+        /*
+        * CUANDO LA APLICACIÓN EMITE LA SEÑAL DE QUE SE VA A DEJAR DE EJECUTAR,
+        * ELIMINA EL HILO, ESPERA A QUE SE ELIMINE Y LUEGO YA DEJA DE EJECUTAR LA
+        * APLICACIÓN
+        */
         connect(qApp, &QApplication::aboutToQuit, thread, [this, thread] () {
             thread->quit();
             thread->wait();
