e6ad5b04acf6f362259caccb729f26a464bfacd1
AlejandroRodriguez05 <alejandrorodriguezsanchez2005@gmail.com>
2026-02-06 12:07:12 +0000

    feat: Implementación de QRandomGenerator para el tiempo de instalacion y desinstalacion de cada app dentro de installerworker.

--- application-core/installerworker.cpp
+++ application-core/installerworker.cpp
@@ -1,5 +1,6 @@
 #include "installerworker.h"
 #include <QThread>
+#include <QRandomGenerator>
 
 installerWorker::installerWorker(QModelIndex sourceIndex, QObject* parent) :
     QObject(parent),
@@ -9,10 +10,12 @@ installerWorker::installerWorker(QModelIndex sourceIndex, QObject* parent) :
 
 void installerWorker::install(){
 
+    quint32 tiempo = QRandomGenerator::global()->bounded(50, 101);
+
     //EL BUCLE QUE SIMULA EL PROGRESO DE LA PROGRESSBAR
     for (int i=0; i<=100; i++){
         //PARAMOS EL HILO POR CADA VUELTA DEL FOR PARA QUE SE VEA EN LA UI
-        QThread::msleep(50);
+        QThread::msleep(tiempo);
         //EMITIMOS EL ID Y EL PROGRESO QUE LLEVA
         emit progress(mSourceIndex, i);
     }
